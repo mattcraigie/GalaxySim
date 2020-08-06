976544c1c77f2dfece292239436f2277ee2e4dd2
mattcraigie <matthewcraigie1@gmail.com>
2020-08-06 15:20:19 +1000

    added functioning gravity

--- simulation.py
+++ simulation.py
@@ -6,12 +6,21 @@ mpl.rcParams['animation.ffmpeg_path'] = r'C:\\Program Files (x86)\\FFMpeg\\bin\\
 
 np.random.seed(4080)
 
+# possible extensions: gaussian (or otherwise) mass distribution for each particle
+
+
 # Setup
 n_dimensions = 2
 n_particles = 5
 n_frames = 100
 frame_duration = 100
 v_max = 0.01  # maximum drift velocity (distance per timestep)
+dt = 1
+
+# physical quantities
+particle_mass = 1
+radius_buffer = 0.1
+G = 1e-4
 
 # Set initial positions and velocities
 position = 1 - 2 * np.random.random((n_dimensions, n_particles))
@@ -23,6 +32,10 @@ def apply_boundary(p):
     todo: un-hard code the periodic number
     """
     # i is x, y, z while j is particle 1, particle 2...
+
+    # p[p > 1] -= 2
+    # p[p < 1] += 2
+
     for i in range(n_dimensions):
         for j in range(n_particles):
             if p[i, j] > 1:
@@ -34,12 +47,24 @@ def apply_boundary(p):
 
 def apply_force(p, v):
     """Defines the interactions - we want a softening length to avoid crazy big forces. It should be ~ the mean
-    separation length of two particles of mass M"""
+    separation length of two particles of mass M.
+
+    the change in velocity (i.e. acceleration) particle m is determined by its interaction with n
+    """
 
 
-    for j in range(n_particles):
+    for m in range(n_particles):
         # particle-particle interaction
-        pass
+        distance_factor = 0
+
+        for n in range(n_particles):
+            if n == m:
+                continue
+            distance_factor += (p[:, m] - p[:, n]) / (np.linalg.norm(p[:, m] - p[:, n])**n_dimensions + radius_buffer)
+        v[:, m] += - G * distance_factor
+
+    return p, v
+
     # normal denominator plus a small num
 
 # Set the axes on which the points will be shown
@@ -56,14 +81,14 @@ points, = plt.plot([], [], 'o', markersize=5)
 # Define procedure to update positions at each timestep
 def update(i):
     global position, velocity
-    position += velocity  # Increment positions according to their velocites
+    position += velocity * dt  # Increment positions according to their velocites
     position = apply_boundary(position)  # Apply boundary conditions
+    position, velocity = apply_force(position, velocity)
     points.set_data(position[0, :], position[1, :])  # Show 2D projection of first 2 position coordinates
     return points,
 
 
-
-ani = FuncAnimation(fig, update, interval=300)
+ani = FuncAnimation(fig, update, frames=n_frames)
 
 
 f = r"sim.mp4"
