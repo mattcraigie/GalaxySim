3d238caab3d1ab8f0d0f62969bca485b57d73c72
mattcraigie <matthewcraigie1@gmail.com>
2020-08-08 10:22:12 +1000

    updated to physical units

--- simulation.py
+++ simulation.py
@@ -4,42 +4,47 @@ from matplotlib.animation import FuncAnimation, FFMpegWriter
 import matplotlib as mpl
 mpl.rcParams['animation.ffmpeg_path'] = r'C:\\Program Files (x86)\\FFMpeg\\bin\\ffmpeg.exe'
 
-np.random.seed(4080)
+np.random.seed(42)
 
-# possible extensions: gaussian (or otherwise) mass distribution for each particle
+# todo: physical units
+# todo: introduce PM approach
+# todo: force wrapping
+# todo: gaussian (or otherwise) mass distribution for each particle
 
+km_to_pc = 3.240e-14
 
 # Sim setup
 n_dimensions = 3
 n_particles = 10
-n_frames = 300
+n_frames = 1000
 frame_duration = 100
-dt = 1
 
-# physical quantities
-particle_mass = 1
-radius_buffer = 0.1
-G = 1e-4
-v_max = 0.03  # starting maximum velocity for the particles
-side_length = 1
+
+# physical quantities - everything SI
+particle_mass = 10**11 * 1.98e30            # kg
+G = 6.67e-11                                # m^3 kg^-1 s^-2
+v_max = 1000 * 1000                         # m/s
+side_length = 1e5 * 3.086e16                # in units of m
+radius_buffer = 0.1 * side_length           # in units of m
+dt = 3.154e+7 * 1e5                        # in units of seconds
 
 
 # Set initial positions and velocities
-position = 0.5 * side_length - side_length * np.random.random((n_dimensions, n_particles))
-velocity = v_max * (0.5 * side_length - side_length * np.random.random((n_dimensions, n_particles)))
+position = side_length * (0.5 - np.random.random((n_dimensions, n_particles)))
+velocity = v_max * (0.5 - np.random.random((n_dimensions, n_particles)))
 
 
 def apply_boundary(p):
-    """Defines the boundary conditions - we want a periodic boundary
-    """
+    """Defines the boundary conditions - we want a periodic boundary"""
     p[p > side_length/2] -= side_length
     p[p < -side_length/2] += side_length
-
     return p
 
 
 def apply_force(p, v):
-    """Defines the interactions - we want a softening length to avoid crazy big forces. It should be ~ the mean
+    """Returns the velocity in km/s (weird)
+
+    Defines the interactions - we want a softening length to avoid crazy big forces. It should be ~ the mean
     separation length of two particles of mass M.
 
     the change in velocity (i.e. acceleration) particle m is determined by its interaction with n
@@ -48,13 +53,13 @@ def apply_force(p, v):
     for m in range(n_particles):
         # particle-particle interaction
         distance_factor = 0
-
         for n in range(n_particles):
             if n == m:
                 continue
-            distance_factor += (p[:, m] - p[:, n]) / (np.linalg.norm(p[:, m] - p[:, n])**n_dimensions + radius_buffer)
-        v[:, m] += - G * distance_factor * dt
 
+            distance_factor += (p[:, m] - p[:, n]) / (np.linalg.norm(p[:, m] - p[:, n])**n_dimensions + radius_buffer)
+        # this might also need some 4s and pis etc.
+        v[:, m] += - G * particle_mass * distance_factor * dt
     return p, v
 
 
