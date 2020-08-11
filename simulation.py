3a98fdb9194bcde9ab43f779051aa297d0bc9f7f
mattcraigie <matthewcraigie1@gmail.com>
2020-08-11 14:27:23 +1000

    halfway to particle-mesh

--- simulation.py
+++ simulation.py
@@ -10,6 +10,7 @@ np.random.seed(42)
 # todo: introduce PM approach
 # todo: force wrapping
 # todo: gaussian (or otherwise) mass distribution for each particle
+# todo: PM to arbitrary dimension
 
 km_to_pc = 3.240e-14
 
@@ -26,7 +27,7 @@ G = 6.67e-11                                # m^3 kg^-1 s^-2
 v_max = 1000 * 1000                         # m/s
 side_length = 1e5 * 3.086e16                # in units of m
 radius_buffer = 0.1 * side_length           # in units of m
-dt = 3.154e+7 * 1e5                        # in units of seconds
+dt = 3.154e+7 * 1e5                         # in units of seconds
 
 
 # Set initial positions and velocities
@@ -41,7 +42,7 @@ def apply_boundary(p):
     return p
 
 
-def apply_force(p, v):
+def apply_force_particle(p, v):
     """Returns the velocity in km/s (weird)
 
     Defines the interactions - we want a softening length to avoid crazy big forces. It should be ~ the mean
@@ -63,6 +64,42 @@ def apply_force(p, v):
     return p, v
 
 
+def apply_force_particle_mesh(p, v):
+    grid_num = 50
+    x = np.linspace(-1, 1, 50)
+    y = np.linspace(-1, 1, 50)
+    z = np.linspace(-1, 1, 50)
+    pos_grid = np.meshgrid(x, y, z)
+    mass_grid = np.zeros([grid_num] * n_dimensions)
+    H = side_length / grid_num
+
+    print(pos_grid)
+    print(mass_grid)
+    exit()
+
+    for i in range(n_particles):
+        diff = p[:, i] - pos_grid
+        p[:, i]
+
+    rho_grid = mass_grid / H**3
+
+def pm_mass_assignment_func(m, g, H):
+
+
+
+
+    return m * W
+
+def ngp(d, H):
+    if d <= H/2:
+        return 1
+    else:
+        return 0
+
+
+
+
+
 # Set the axes on which the points will be shown
 plt.ion() # Set interactive mode on
 fig, ax = plt.subplots(figsize=(8, 8))
@@ -79,7 +116,7 @@ def update(i):
     global position, velocity
     position += velocity * dt  # Increment positions according to their velocites
     position = apply_boundary(position)  # Apply boundary conditions
-    position, velocity = apply_force(position, velocity)
+    position, velocity = apply_force_particle_mesh(position, velocity)
     points.set_data(position[0, :], position[1, :])  # Show 2D projection of first 2 position coordinates
     return points,
 
