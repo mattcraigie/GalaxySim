e10ab95a354a8cc4822cf7c40120121022d4bce3
mattcraigie <matthewcraigie1@gmail.com>
2020-08-19 12:55:27 +1000

    functioning p3m

--- simulation.py
+++ simulation.py
@@ -17,8 +17,8 @@ km_to_pc = 3.240e-14
 
 # Sim setup
 n_dimensions = 3
-n_particles = 500
-n_frames = 1000
+n_particles = 1000
+n_frames = 100
 frame_duration = 100
 
 
@@ -32,10 +32,10 @@ frame_duration = 100
 
 # de-uniting
 particle_mass = 1            # kg
-G = 1                           # m^3 kg^-1 s^-2
+G = 0.01                           # m^3 kg^-1 s^-2
 v_max = 0.05                         # m/s
 side_length = 1                # in units of m
-radius_buffer = 0.1 * side_length           # in units of m
+radius_buffer = 0.05 * side_length           # in units of m
 dt = 0.005                         # in units of seconds
 
 
@@ -52,13 +52,6 @@ def apply_boundary(p):
 
 
 def apply_force_pp(p, v):
-    """Returns the velocity in km/s (weird)
-
-    Defines the interactions - we want a softening length to avoid crazy big forces. It should be ~ the mean
-    separation length of two particles of mass M.
-
-    the change in velocity (i.e. acceleration) particle m is determined by its interaction with n
-    """
 
     for m in range(n_particles):
         # particle-particle interaction
@@ -67,10 +60,14 @@ def apply_force_pp(p, v):
             if n == m:
                 continue
 
-            distance_factor += (p[:, m] - p[:, n]) / (np.linalg.norm(p[:, m] - p[:, n])**n_dimensions + radius_buffer)
+            distance_factor += pp_displacement(p[:, m], p[:, n])
         # this might also need some 4s and pis etc.
         v[:, m] += - G * particle_mass * distance_factor * dt
     return p, v
+
+
+def pp_displacement(p1, p2):
+    return (p1 - p2) / (np.linalg.norm(p1 - p2) ** n_dimensions + radius_buffer)
 
 
 def apply_force_pm(p, v):
@@ -108,8 +105,81 @@ def apply_force_pm(p, v):
     for j in range(n_particles):
         j_coords = particle_coords[:, j]
         accel = a_grid[:, particle_coords[0, j], particle_coords[1, j], particle_coords[2, j]]
-        
         v[:, j] += accel * dt
+    return p, v
+
+
+def apply_force_p3m(p, v):
+    cell_num = 50
+    particle_coords = get_particle_coords(p, cell_num)
+
+    # assigning into grid
+    all_cells = np.empty([cell_num, cell_num, cell_num], dtype=list)
+
+    for i in range(n_particles):
+        particle_coord = particle_coords[:, i]
+        if all_cells[tuple(particle_coord)] is None:
+            all_cells[tuple(particle_coord)] = [i]
+        else:
+            all_cells[tuple(particle_coord)] += [i]
+
+    rho = ngp(particle_coords, cell_num)
+    rho_ft = rfftn(rho, axes=(0,1,2))
+
+    x = (np.linspace(-0.5, 0.5, cell_num + 1) + 0.5 / cell_num)[:-1] * side_length
+    pos_grid = np.array(np.meshgrid(x, x, x))
+    k_grid = 2 * np.pi * pos_grid / side_length
+    k_norm = np.linalg.norm(k_grid, axis=0)
+
+    # halving bc rfft is funny
+    half = int(cell_num / 2 + 1)
+    k_grid = k_grid[:, :, :, :half]
+    k_norm = k_norm[:, :, :half]
+
+    a_ft_grid = 4 * 1j * np.pi * G * rho_ft * k_grid / k_norm**2
+    a_grid = irfftn(a_ft_grid, axes=(1, 2, 3))
+
+    # a_mag = np.linalg.norm(a_grid, axis=0)
+    # to_plot = np.sum(a_mag, axis=2)
+    #
+    # plt.close()
+    # plt.imshow(to_plot)
+    # plt.savefig('accelfield.png')
+    #
+    # exit()
+
+    # inside the range, subtract the mesh force and add the distance force
+    # outside the range, do nothing
+
+    for j in range(n_particles):
+        j_coords = particle_coords[:, j]
+        pm_accel = a_grid[:, j_coords[0], j_coords[1], j_coords[2]]
+
+        cell_depth = 3
+
+        local_cells = np.array([[j_coords[0] + l, j_coords[1] + m, j_coords[2] + n]
+                       for l in range(-cell_depth, cell_depth, 1)
+                       for m in range(-cell_depth, cell_depth, 1)
+                       for n in range(-cell_depth, cell_depth, 1)])
+
+        local_cells[local_cells >= 50] -= 50
+        local_cells[local_cells <= -1] += 50
+
+        distance_factor = 0
+        for f in range(np.shape(local_cells)[1]):
+            cell_idx = tuple(local_cells[f])
+            if all_cells[cell_idx] is None:
+                continue
+            else:
+                nearby = all_cells[cell_idx]
+                cell_pos = ((local_cells[f]) / cell_num - 0.5) * side_length
+                for nb in nearby:
+                    distance_factor += pp_displacement(p[:, j], p[:, nb])
+                    distance_factor -= pp_displacement(p[:, j], cell_pos)
+
+        pp_accel = - G * particle_mass * distance_factor
+        v[:, j] += (pm_accel + pp_accel) * dt
+
     return p, v
 
 
@@ -182,7 +252,7 @@ def update(i):
     # print("")
     # print(position, velocity)
     apply_force_pp(position, velocity)
-    position, velocity = apply_force_pm(position, velocity)
+    position, velocity = apply_force_p3m(position, velocity)
     # print("")
     # print(position, velocity)
     points.set_data(position[1, :], position[2, :])  # Show 2D projection of first 2 position coordinates
@@ -200,7 +270,7 @@ if __name__ == '__main__':
     points, = plt.plot([], [], 'o', markersize=5)
 
     ani = FuncAnimation(fig, update, frames=n_frames)# interval=frame_duration)
-    f = r"simpp.mp4"
+    f = r"sim_p3m.mp4"
     writervideo = FFMpegWriter(fps=60)
     ani.save(f, writer=writervideo)
 
